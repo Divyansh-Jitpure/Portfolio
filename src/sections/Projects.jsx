a9bb7dfd61840f24050b205037304457594743a6
Divyansh Jitpure <divyanshjit2022@gmail.com>
2024-08-01 00:56:38 +0530

    Added EditR

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -7,17 +7,24 @@ const Projects = ({ projectsRef }) => {
       <h2 className="mb-5 text-2xl font-medium text-cyan-300">
         <u>Projects</u>
       </h2>
+
       <Project
-        title="CooPalette"
-        description="COOPALETTE is a web-based color palette generator that offers an endless array of color combinations and their corresponding Hex Codes. Whether you're a designer seeking inspiration or a developer looking to enhance visual aesthetics, COOPALETTE has you covered."
-        projectLink="https://coopalette.web.app/"
-        techStack={["HTML", "CSS", "JavaScript", "Google Firebase"]}
+        title="EditR - Image Editor"
+        description="EditR! An image editor built with React, Tailwind CSS, and Firebase. 🎨 EditR offers filter tools like brightness, contrast, saturation, and exposure, plus transform tools such as rotate and flip. Enjoy a responsive UI and dynamic functionality. Edit, transform, and download your images effortlessly!"
+        projectLink="https://the-editr.web.app/"
+        techStack={["React JS", "Tailwind CSS", "Google Firebase"]}
       />
       <Project
         title="Divyansh Jitpure - Portfolio"
         description="My first Portfolio website built with React, Tailwind, ThreeJS etc. It showcases everything about my Skills, Experience and projects."
         projectLink="/"
         techStack={["React JS", "Tailwind CSS", "Three JS", "Google Firebase"]}
+      />
+      <Project
+        title="CooPalette"
+        description="COOPALETTE is a web-based color palette generator that offers an endless array of color combinations and their corresponding Hex Codes. Whether you're a designer seeking inspiration or a developer looking to enhance visual aesthetics, COOPALETTE has you covered."
+        projectLink="https://coopalette.web.app/"
+        techStack={["HTML", "CSS", "JavaScript", "Google Firebase"]}
       />
       <Project
         title="Games Of Web"
